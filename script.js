b3cd7f660d204705d3c72cbcc8b23896b6fc820d
Swarup Tripathy <mstrider10@gmail.com>
2021-12-15 19:11:30 +0530

    add class hours

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
-const d = new Date();
+const d = new Date();   //creating an object for the date
 document.getElementById("hours").innerHTML=d;
+
+//7 numbers specify year, month, day, hour, minute, second, and millisecond (in that order)
+//here month starts from 0(jan) and ends at 11(dec)
+const c = new Date(2030,7,4,22,15,0);
+document.getElementById("hours2").innerHTML=c;
