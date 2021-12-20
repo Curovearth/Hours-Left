effbfbeaf0d485a1613676dfb67149885cefd7f8
Swarup Tripathy <mstrider10@gmail.com>
2021-12-20 17:53:54 +0530

    Timer is On

--- script.js
+++ script.js
@@ -1,19 +1,14 @@
-
-
 //7 numbers specify year, month, day, hour, minute, second, and millisecond (in that order)
 //here month starts from 0(jan) and ends at 11(dec)
-
-
 
 
 //Building a function to update the countdown in every milisecond
 
 var x = setInterval(function(){
-    var c = new Date("Aug 4, 2025 22:15");
-    var d = new Date();   //creating an object for the date i.e., todays' date
-    // var c = new Date(2022,0,1);       //rough date of new year
-
-    var diff = c.getTime()-d.getTime();     //returns the number of miliseconds left
+    // var c = new Date("Aug 4, 2025 22:55");
+    var d = new Date();     //creating an object for the date i.e., todays' date
+    var c = new Date(2022,0,1);       //rough date of new year
+    var diff = c-d;     //returns the number of miliseconds left
     document.getElementById("demo").innerHTML=diff;
 },1);
 
